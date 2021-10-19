a40cccb3a0e84fa8d49651d1fa60367165741a17
Julia Aasmaa <my_e_10@hotmail.com>
2021-10-19 11:27:33 +0300

    functions

---
+++ Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta esimene number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sisesta teine number:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Vali tehe ('+', '-', '*' või '/'");
+            char userOperation = Convert.ToChar(Console.ReadLine());
+
+            switch (userOperation)
+            {
+                case '+':
+                    AddTwoNumbers(firstNumber, secondNumber);
+                    break;
+                case '/':
+                    DivideTwoNumbers(firstNumber, secondNumber);
+                    break;
+                default:
+                    Console.WriteLine("vale tehe");
+                    break;
+            }
+
+
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+        
+        private static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine($"{x} / {y} = {result}");
+        }
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma nimi:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            SayHello(userName, userAge);
+        }
+
+        private static void SayHello(string someName, int someNumber)
+        {
+            Console.WriteLine($"Tere, {someName}!");
+            Console.WriteLine($"Oled sündinud aastal {2021 - someNumber}.");
+        }
+
+
+
+    }
+}
